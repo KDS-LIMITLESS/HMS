e1b7f20fd786e0c8ff85b12e574783d8fc6b8304
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-06-29 15:48:16 +0100

    added port to config vars

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import dotenv from "dotenv"
 import { dbConnection } from './connection';
 import { createItemsTable } from './models/item';
 import { createOrderTable } from './models/order';
@@ -6,6 +7,7 @@ import { createUsersTable } from './models/user';
 
 
 const app = express();
+dotenv.config()
 
 app.use(express.json())
 app.use('', require('./routes/item'));
@@ -23,8 +25,9 @@ async function startServer(){
     //await createOrderTable();
     await createUsersTable();
     //await db.query(`DROP TABLE ordert`)
+    const PORT = process.env.PORT || 3000
 
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
         console.log('Server Listening on port 3000')
     })
 }
