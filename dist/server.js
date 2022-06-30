98f492202909731dd7935378e2a62d8243d1222b
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-06-30 12:01:24 +0100

    Dropping users table

--- dist/server.js
+++ dist/server.js
@@ -31,7 +31,7 @@ function startServer() {
         // await createItemsTable().then(() => console.log("done creating items tables"));
         // await createOrderTable().then(() => console.log("done creating order table"));
         // await createUsersTable().then(() => console.log("done creating user table")); 
-        // await db.query(`DROP TABLE users`)
+        yield db.query(`DROP TABLE users`);
         const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log('Server Listening on port 3000');
