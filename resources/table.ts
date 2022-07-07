bfe561d2037d8f99cb6c0c24af1dd4a35c7de25f
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-07-07 09:34:10 +0100

    making table ordering functionality

--- resources/table.ts
+++ resources/table.ts
@@ -5,7 +5,7 @@ import { create_new_table } from "../models/table";
 export async function createTable(req:Request, res: Response, next: NextFunction) {
     try {
         console.log(JSON.stringify(req.body) + ' From createTable function...')
-        let result = await create_new_table(req.body.table_name, req.body.activeUser)
+        let result = await create_new_table(JSON.stringify(req.body.table_name), req.body.activeUser)
         console.log(`Created table ${result.rows[0]['table_name']}` )
         
         next();
