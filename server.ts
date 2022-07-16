5d7301a1fc7bb9a8a9db776778cdd2b7e7eed6b9
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-07-16 16:47:37 +0100

    ...

--- server.ts
+++ server.ts
@@ -32,9 +32,10 @@ async function startServer(){
     // await createTableManager();
     // await create_Order_Table().then(() => console.log("done creating order table"));
 
-    // await db.query(`DROP TABLE orders`)
+    await db.query(`DROP TABLE orders`)
+    
+    await db.query(`DROP TABLE tables`)
     // await db.query(`DROP TABLE item`)
-    // await db.query(`DROP TABLE tables`)
     // await db.query(`DROP TABLE users`)
 
     // await db.query(`ALTER TABLE tables DROP COLUMN payment_method`)
