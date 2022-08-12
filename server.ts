4d4c6b59dc987460928f9f0574af182d0beff7d0
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-08-12 14:30:46 +0100

    Creating tables in dbase database

--- server.ts
+++ server.ts
@@ -34,9 +34,12 @@ app.listen(PORT, () => {
 })
 
 async function startServer() {
-    // await createTableManager()
-    // await create_notifications_table();
-    // await create_Order_Table()
+    await createTableManager()
+    await create_notifications_table();
+    await create_Order_Table()
+    await create_credit_table()
+    await createUsersTable()
+    await createItemsTable();
 
     // await db.query(`ALTER TABLE notification 
     //     DROP CONSTRAINT notification_waiter_fkey,
