dc8eba76b481c94a63a68b431b944027ce3a954e
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-08-12 14:18:32 +0100

    configuring databse connections

--- connection.ts
+++ connection.ts
@@ -5,25 +5,25 @@ dotenv.config()
 
 export let db = new Pool();
 
-if (process.env.RDS_HOSTNAME || process.env.DATABASE_URL) {
-	console.log(`Running Production Databse......`)
-	db = new Pool({
-		host: process.env.RDS_HOSTNAME,
-		port: 5432,
-		database: process.env.RDS_DB_NAME,
-		user: process.env.RDS_USERNAME,
-		password: process.env.RDS_PASSWORD
-	}) 
-} else {
-	console.log(`Running Development Databse......`)
-	db = new Pool({
-	  user: "hope",
-	  host: "localhost",
-	  database: "ballroom",
-	  password: "12345678kds",
-	  port: 5432
-	});
-}
+// if (process.env.RDS_HOSTNAME || process.env.DATABASE_URL) {
+// 	console.log(`Running Production Databse......`)
+// 	db = new Pool({
+// 		host: process.env.RDS_HOSTNAME,
+// 		port: 5432,
+// 		database: process.env.RDS_DB_NAME,
+// 		user: process.env.RDS_USERNAME,
+// 		password: process.env.RDS_PASSWORD
+// 	}) 
+// } else {
+// 	console.log(`Running Development Databse......`)
+// 	db = new Pool({
+// 	  user: "hope",
+// 	  host: "localhost",
+// 	  database: "ballroom",
+// 	  password: "12345678kds",
+// 	  port: 5432
+// 	});
+// }
 
 
 
