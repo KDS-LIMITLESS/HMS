6f412fc1dc4421d8f502e750d8c7af19234c8f65
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-09-08 10:58:05 +0100

    Creating database tables in test server

--- server.ts
+++ server.ts
@@ -43,11 +43,11 @@ async function startServer() {
     // await db.query('DROP TABLE notification')
     
     await createUsersTable()
-    await createItemsTable()
-    await createTableManager()
-    await create_Order_Table()
-    await create_credit_table()
-    await create_notifications_table();
+    // await createItemsTable()
+    // await createTableManager()
+    // await create_Order_Table()
+    // await create_credit_table()
+    // await create_notifications_table();
 
     // await db.query(`ALTER TABLE notification 
     //     DROP CONSTRAINT notification_waiter_fkey,
