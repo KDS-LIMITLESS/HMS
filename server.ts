1e3bb8c697eddb7f57220dea8d351a13ee2b9d06
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-09-13 14:05:22 +0100

    Recreating tables in dbase production databse

--- server.ts
+++ server.ts
@@ -36,11 +36,14 @@ app.listen(PORT, () => {
 
 async function startServer() {
     // await createUsersTable();
+    
     // await createItemsTable();
-    await db.query('DROP TABLE orders')
-    await db.query('DROP TABLE tables')
-    await db.query('DROP TABLE credit')
-    await db.query('DROP TABLE notification')
+    await db.query('DROP TABLE item')
+    await db.query('DROP TABLE users')
+    // await db.query('DROP TABLE orders')
+    // await db.query('DROP TABLE tables')
+    // await db.query('DROP TABLE credit')
+    // await db.query('DROP TABLE notification')
     
     // await createUsersTable()
     // await createItemsTable()
