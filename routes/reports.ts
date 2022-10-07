32e7419c5f032fcfb386442a7e7569210367f207
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-07 18:03:19 +0100

    Added protection to route 'clear-db'

--- routes/reports.ts
+++ routes/reports.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { report, getItemReports, generateOverallReport, clearDbDetails } from '../resources/reports';
 import { uploadReportFile, retrievePDF } from '../middlewares/imageloader';
+import { authorizeSuperAdminNext } from '../middlewares/user';
 
 
 export const router = express.Router();
@@ -11,7 +12,7 @@ router.get('/overall-reports', generateOverallReport)
 router.post('/upload-report', uploadReportFile)
 router.post('/retrieve-pdf', retrievePDF)
 
-router.delete('/clear-db', clearDbDetails)
+router.delete('/clear-db', authorizeSuperAdminNext, clearDbDetails)
 
 
 module.exports = router;
