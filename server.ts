65fcca0a39a9c53e7421a7f163e05964f1fa535c
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-09 18:00:41 +0100

    Dropping and recreating tables to pick up changes to database

--- server.ts
+++ server.ts
@@ -46,17 +46,17 @@ app.listen(PORT, () => {
 
 async function startServer() {
    
-    // await db.query('DROP TABLE orders')
-    // await db.query('DROP TABLE tables')
-    // await db.query('DROP TABLE credit')
-    // await db.query('DROP TABLE notification')
-    // await db.query('DROP TABLE products')
-    // await db.query('DROP TABLE catalogue')
+    await db.query('DROP TABLE orders')
+    await db.query('DROP TABLE tables')
+    await db.query('DROP TABLE credit')
+    await db.query('DROP TABLE notification')
+    await db.query('DROP TABLE products')
+    await db.query('DROP TABLE catalogue')
     
-    // await db.query('DROP TABLE item')
-    // await db.query('DROP TABLE dept')
-    // // await db.query('DROP TABLE users')
-    // 
+    await db.query('DROP TABLE item')
+    await db.query('DROP TABLE dept')
+    // await db.query('DROP TABLE users')
+    
     // await createUsersTable()
     await createDeptTable()
     await createItemsTable()
@@ -65,6 +65,7 @@ async function startServer() {
     await create_Order_Table()
     await create_credit_table()
     await create_notifications_table();
+    await create_inventory_order_table()
   
 }
 startServer();
