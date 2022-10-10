427dfcfce7fa673093212f77c33a811a7243d428
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-10 09:56:56 +0100

    ...

--- server.ts
+++ server.ts
@@ -70,10 +70,9 @@ async function startServer() {
     // await create_notifications_table();
     // await create_inventory_order_table()
 
-   //  await db.query(`ALTER TABLE item
-   //      DROP COLUMN category
-   // `)
-  
+    await db.query(`ALTER TABLE item
+        DROP COLUMN category;
+    `)
     //await db.query(`ALTER TABLE products
     //    ADD COLUMN category VARCHAR NOT NULL
 //      `)
