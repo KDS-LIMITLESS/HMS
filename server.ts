40ebf2a165f541ccc605b735e4a58de9ab071a85
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-11 18:09:32 +0100

    ...

--- server.ts
+++ server.ts
@@ -51,7 +51,7 @@ async function startServer() {
     await db.query('DROP TABLE tables')
     await db.query('DROP TABLE credit')
     await db.query('DROP TABLE notification')
-    await db.query('DROP TABLE products CASCADE')
+    // await db.query('DROP TABLE products CASCADE')
     await db.query('DROP TABLE catalogue CASCADE')
     
     await db.query('DROP TABLE item CASCADE')
