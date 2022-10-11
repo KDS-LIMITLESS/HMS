51c24fc9a4d76b5d50681eb11b0456238da6b322
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-11 11:11:45 +0100

    Added store manager access check to new-item

--- middlewares/user.ts
+++ middlewares/user.ts
@@ -49,8 +49,26 @@ export async function authorizeSuperAdminNext(req: Request, res: Response, next:
     }  
 }
 
+export async function authorizeStoreManager(req: Request, res: Response, next: NextFunction){
+    try {
+        
+        let userExists = await get_user(req.body.activeUser)
+       
+        if (userExists && ( userExists.rows[0]['role'] === 'Store Manager') && (req.body.activePasscode === userExists.rows[0]['passcode']) ){
+            next();
+        }else {
+            console.log(req.body)
+            return res.status(400).send("Please login to continue")
+        }
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(500).send("An error Occured!")
+    }  
+}
+
 export async function authorizeAuditor(req: Request, res: Response, next: NextFunction){
-    const USERS = ['Auditor', 'Store Manager', 'Super Admin', 'Bar Man']
+    const USERS = ['Auditor', 'Super Admin', 'Bar Man']
     try {
         let userExists = await get_user(req.body.activeUser)
         if (userExists && USERS.includes(userExists.rows[0]['role']) 
