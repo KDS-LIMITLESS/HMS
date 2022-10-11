1e3ec9183ea52f76342eaf2d5ad6f1515f58fed4
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-11 18:39:57 +0100

    ...

--- server.ts
+++ server.ts
@@ -53,23 +53,23 @@ async function startServer() {
     // await db.query('DROP TABLE notification')
     // await db.query('DROP TABLE products ')
     // await db.query('DROP TABLE catalogue ')
-    // await db.query('DROP TABLE item ')
+    // await db.query('DROP TABLE item CASCADE')
     // await db.query('DROP TABLE dept ')
     // await db.query('DROP TABLE users')
     
     
 
-    await createUsersTable()
-    await createDeptTable()
-    await createItemsTable()
-
-    await createProductTable()
-    
-    await createTableManager()
-    await create_Order_Table()
-    await create_credit_table()
-    await create_notifications_table();
-    await create_inventory_order_table()
+     await createUsersTable()
+     await createDeptTable()
+     await createItemsTable()
+ 
+     await createProductTable()
+     
+     await createTableManager()
+     await create_Order_Table()
+     await create_credit_table()
+     await create_notifications_table();
+     await create_inventory_order_table()
 
     //await db.query(`ALTER TABLE item
     //    DROP COLUMN category;
