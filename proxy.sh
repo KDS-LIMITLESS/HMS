27681d3854893855fd4d5196d9372c89b8facbe7
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-11-12 05:20:43 +0100

    GCP auth credentials

--- proxy.sh
+++ proxy.sh
@@ -2,4 +2,4 @@
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.386
 mv cloud_sql_proxy.linux.386 cloud_sql_proxy
 chmod +x cloud_sql_proxy
-./cloud_sql_proxy -instances=synthetic-trail-368311:us-central1:uppist-2022-instance=tcp:3306 -credential_file=./synthetic-trail-368311-3817ed4addc5.json
+./cloud_sql_proxy -instances=synthetic-trail-368311:us-central1:uppist-2022-instance=tcp:5432 -credential_file=./synthetic-trail-368311-3817ed4addc5.json
